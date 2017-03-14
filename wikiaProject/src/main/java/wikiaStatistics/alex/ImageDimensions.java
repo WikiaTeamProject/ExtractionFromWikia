5083022eef21ff6cdcc0452206ddb8d7073a573d
alexandrahofmann <alexandra_hofmann@gmx.de>
2017-03-14 18:54:25 +0100

    extract wikis and save as csv

---
+++ wikiaProject/src/main/java/wikiaStatistics/alex/ImageDimensions.java
@@ -0,0 +1,46 @@
+package wikiaStatistics.alex;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by alexandrahofmann on 14.03.17.
+ */
+public class ImageDimensions {
+
+    @JsonProperty("width")
+    int width = 0;
+
+    @JsonProperty("height")
+    int height = 0;
+
+
+    public ImageDimensions() {
+    }
+
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return width + ";" + height;
+    }
+
+    public static String getHeader() {
+        return  "width" +
+                ";height";
+    }
+}
---
+++ wikiaProject/src/main/java/wikiaStatistics/alex/WikiaStats.java
@@ -0,0 +1,67 @@
+package wikiaStatistics.alex;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by alexandrahofmann on 14.03.17.
+ */
+
+public class WikiaStats {
+
+    @JsonProperty("users")
+    int users = 0;
+
+    @JsonProperty("articles")
+    int articles = 0;
+
+    @JsonProperty("pages")
+    int pages = 0;
+
+    @JsonProperty("admins")
+    int admins = 0;
+
+    @JsonProperty("activeUsers")
+    int activeUsers = 0;
+
+    @JsonProperty("edits")
+    int edits = 0;
+
+    @JsonProperty("videos")
+    int videos = 0;
+
+    @JsonProperty("images")
+    int images = 0;
+
+    @JsonProperty("discussions")
+    int discussions = 0;
+
+
+    public WikiaStats() {
+    }
+
+
+    @Override
+    public String toString() {
+        return  users +
+                ";" + articles +
+                ";" + pages +
+                ";"  + admins +
+                ";"  + activeUsers +
+                ";" + edits +
+                ";" + videos +
+                ";" + images +
+                ";" + discussions;
+    }
+
+    public static String getHeader() {
+        return  "users" +
+                ";articles" +
+                ";pages" +
+                ";admins" +
+                ";activeUsers" +
+                ";edits" +
+                ";videos" +
+                ";images" +
+                ";discussions";
+    }
+}
