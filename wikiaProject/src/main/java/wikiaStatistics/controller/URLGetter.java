08c1338ee91f79b25519c77c51f58e4b8f73dd69
alexandrahofmann <alexandra_hofmann@gmx.de>
2017-03-20 12:59:24 +0100

    replace appearing semikolon with komma

--- wikiaProject/src/main/java/wikiaStatistics/controller/URLGetter.java
+++ wikiaProject/src/main/java/wikiaStatistics/controller/URLGetter.java
@@ -131,7 +131,7 @@ public class URLGetter implements Runnable {
                         for (String id : ids) {
                             // map json string with infos of one wiki to java object
                             ExpandedWikiaItem wiki = mapper.readValue(items.getJSONObject(id).toString(), ExpandedWikiaItem.class);
-                            bufferedWriter.write(wiki.toString().replace("\n", "").replace("\r", "") + "\n"); // line breaks have to be deleted
+                            bufferedWriter.write(wiki.toString().replace(";",",").replace("\n", "").replace("\r", "") + "\n"); // line breaks have to be deleted
                             // logger.info(id); // leads to a lot of output on the console
                         }
                     }
