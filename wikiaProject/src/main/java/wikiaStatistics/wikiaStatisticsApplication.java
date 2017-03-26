51f1461dd223ed42735077f4e256238abbf1b7d4
alexandrahofmann <alexandra_hofmann@gmx.de>
2017-03-26 17:14:30 +0200

    excluded wikiaOverview files

--- wikiaProject/src/main/java/wikiaStatistics/wikiaStatisticsApplication.java
+++ wikiaProject/src/main/java/wikiaStatistics/wikiaStatisticsApplication.java
@@ -1,13 +1,8 @@
 package wikiaStatistics;
 
-import wikiaStatistics.controller.MetadataGetter;
 import wikiaStatistics.controller.MetadataGetterImpl;
 import wikiaStatistics.model.MetadataStatistics;
 import wikiaStatistics.util.WikiaStatisticsTools;
-
-import java.util.ResourceBundle;
-import java.util.logging.Logger;
-import java.io.*;
 
 /**
  * This class represents the application to download metadata of all Wikia wikis and receive statistics.
@@ -19,7 +14,7 @@ public class wikiaStatisticsApplication {
     public static void main(String[] args) {
 
         // metadata will be downloaded and files saved in the specified directory
-//        MetadataGetterImpl.downloadWikiaMetadata();
+        MetadataGetterImpl.downloadWikiaMetadata();
 
         // statistics of the extracted wikis will be performed
         MetadataStatistics statistics = WikiaStatisticsTools.getMetadataStatistics();
