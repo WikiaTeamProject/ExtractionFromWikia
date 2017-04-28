37f0248ba4b0044389d9b89e59d733f07f882b57
samreshperchani <samreshperchani@hotmail.com>
2017-04-28 21:54:39 +0200

    Added methods to create mapping of extracted entities with newly created namespace

---
+++ wikiaProject/src/main/java/wikiaStatistics/util/EntitiesMapping.java
@@ -0,0 +1,101 @@
+package wikiaStatistics.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by Samresh Kumar on 4/28/2017.
+ */
+public class EntitiesMapping {
+
+    private static Logger logger = Logger.getLogger(WikiaStatisticsTools.class.getName());
+
+    /**
+     *
+     * @param pathToExtractedFiles
+     */
+    public void extractWikiaDbpediaEntitiesMapping(String pathToExtractedFiles){
+        try{
+
+            File extractedFilesPath = new File(pathToExtractedFiles);
+
+            String targetNameSpace= ResourceBundle.getBundle("config").getString("targetnamespace");
+
+            //get list of extracted files in a folder
+            File[] listOfFiles = extractedFilesPath.listFiles();
+
+            String dbPediaNameSpace = "";
+            String mappingFileContents="";
+
+
+            for (int i = 0; i < listOfFiles.length; i++) {
+
+                if (listOfFiles[i].isFile() && listOfFiles[i].toString().endsWith(".ttl") ) {
+
+                    String line = "";
+                    String fileContents = "";
+                    String languageCode = "";
+
+
+                    FileReader fr = new FileReader(listOfFiles[i].getAbsolutePath());
+                    BufferedReader br = new BufferedReader(fr);
+
+                    while ((line = br.readLine()) != null) {
+
+                        if(!line.trim().toLowerCase().startsWith("#") || !line.trim().toLowerCase().startsWith("#")) {
+                            dbPediaNameSpace = line.trim().substring(0, line.indexOf(" "));
+
+                            mappingFileContents += dbPediaNameSpace.replace("dbpedia.org", targetNameSpace) + "<owl:As>" + dbPediaNameSpace + "\n";
+                        }
+
+                    }
+                    br.close();
+                    fr.close();
+
+                }
+            }
+
+
+            //Write Contents to Mapping File
+            writeContentsToMappingFile(mappingFileContents);
+
+        }
+        catch(Exception exception){
+            logger.severe(exception.toString());
+        }
+    }
+
+
+
+    /**
+     *
+     * @param fileContents
+     */
+    public void writeContentsToMappingFile(String fileContents){
+
+        String directoryPath = ResourceBundle.getBundle("config").getString("directory");
+        String mappingFileName=ResourceBundle.getBundle("config").getString("mappingfilename");
+        String filePath = directoryPath + "/"+mappingFileName;
+
+        try{
+            //Initialize file Writer Objects
+            PrintWriter fileWriter = new PrintWriter(filePath, "UTF-8");
+
+            //Write contents to file
+            fileWriter.write(fileContents);
+
+            //Close file Writer
+            fileWriter.close();
+        }
+        catch(Exception exception){
+
+            logger.log(Level.SEVERE, exception.getMessage());
+        }
+    }
+
+}
