22d652a69dfa7fd1dcdf69cdd717592755062404
janothan <janothan@users.noreply.github.com>
2017-04-29 15:03:52 +0200

    Bz2 Extractor finished.

--- wikiaProject/src/main/java/extractionPostprocessing/ExtractionPostprocessingApplication.java
+++ wikiaProject/src/main/java/extractionPostprocessing/ExtractionPostprocessingApplication.java
@@ -9,9 +9,23 @@ public class ExtractionPostprocessingApplication {
 
     public static void main(String[] args) {
 
-        String testFile = "C:\\Users\\D060249\\Desktop\\TMP\\GoT_Wikia_Dump\\enwiki\\20170331\\enwiki-20170331-article-templates-nested.ttl.bz2";
-        String newFile = "C:\\Users\\D060249\\Desktop\\enwiki-20170331-article-templates-nested.ttl";
-        ExtractionBz2.extract(testFile, newFile);
+
+        // Example on how to call the extractor for one file
+        /**
+        String testFile2 = "C:/Users/D060249/Desktop/TMP/GoT_Wikia_Dump/enwiki/20170331/enwiki-20170331-article-templates-nested.ttl.bz2";
+        String newFile2 = "C:/Users/D060249/Desktop";
+        ExtractionBz2.extract(testFile2, newFile2);
+        **/
+
+        // Example on how to call the extractor for multiple files
+        /**
+        String testFile3 = "C:/Users/D060249/Desktop/TMP/GoT_Wikia_Dump/enwiki/20170331/";
+        String newFile3 = "C:/Users/D060249/Desktop/got_wiki";
+
+        ExtractionBz2.extractExtractorResultFiles(testFile3, newFile3);
+         **/
+
     }
+
 
 }
--- wikiaProject/src/main/java/utils/ExtractionBz2.java
+++ wikiaProject/src/main/java/utils/ExtractionBz2.java
@@ -2,23 +2,54 @@ package utils;
 
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Jan Portisch on 29.04.2017.
+ * This class can extract bz2 files.
  */
 public class ExtractionBz2 {
 
     private static Logger logger = Logger.getLogger(ExtractionBz2.class.getName());
 
-
+    /**
+     * Extract one bz2 File
+     * @param pathToFileToExtract The path to the bz2 compressed file that shall be extracted.
+     * @param pathToNewDirectory Path to where the new file shall be written (without the new filename). The new file name will be derived from the old file name.
+     */
     public static void extract(String pathToFileToExtract, String pathToNewDirectory){
         try {
+
+            // get file name
+            String regularExpressionToGetFileNameForwardSlash = "(?<=\\/)[^\\/]*$"; // plain: (?<=\/)[^\/]*$
+            String regularExpressionToGetFileNameBackwardSlash = "(?<=\\\\)[^\\\\]*$"; // plain (?<=\\)[^\\]*$
+            String fileName ="default.ttl.bz2"; // for the name of the new file, use default if it cannot be extracted
+
+            Pattern pattern = Pattern.compile(regularExpressionToGetFileNameForwardSlash);
+            Matcher matcher = pattern.matcher(pathToFileToExtract);
+            if (matcher.find()) {
+                fileName = matcher.group(0);
+            } else {
+                pattern = Pattern.compile(regularExpressionToGetFileNameBackwardSlash);
+                matcher = pattern.matcher(pathToFileToExtract);
+                if (matcher.find()){
+                    fileName = matcher.group(0);
+                }
+            }
+
+            // cut the ".bz2" file ending
+            fileName = fileName.substring(0, (fileName.length()-4));
+
+
+            // do the actual extraction
             FileInputStream in = new FileInputStream(pathToFileToExtract);
-            FileOutputStream out = new FileOutputStream(pathToNewDirectory);
+            FileOutputStream out = new FileOutputStream(pathToNewDirectory + "/" + fileName);
             BZip2CompressorInputStream bzIn = new BZip2CompressorInputStream(in);
             final byte[] buffer = new byte[8192];
             int n = 0;
@@ -33,5 +64,31 @@ public class ExtractionBz2 {
         }
     }
 
+    /**
+     * This method will extract bz2 files specified in pathToFiles and write the decompressed files into the directory pathToNewDirectory.
+     * If the specified target directory does not exist, it will be created.
+     * @param pathToFiles Path to the files that shall be extracted. Only bz2 compressed files will be extracted and written to the new directory specified in pathToNewDirectory.
+     * @param pathToNewDirectory The path where the decompressed files will be written to.
+     */
+    public static void extractExtractorResultFiles(String pathToFiles, String pathToNewDirectory){
+
+        // create pathToNewDirectory if it does not exist yet
+        File newDirectoryPath = new File(pathToNewDirectory);
+        if(!newDirectoryPath.exists()){
+            newDirectoryPath.mkdir();
+        }
+
+        File sourceFilesDirectory = new File(pathToFiles);
+        File[] filesList = sourceFilesDirectory.listFiles();
+        for(File f : filesList){
+            if(f.isFile()){
+
+                if(f.getName().endsWith(".bz2")){
+                    extract(f.getAbsolutePath(), pathToNewDirectory);
+                }
+            }
+        }
+
+    }
 
 }
