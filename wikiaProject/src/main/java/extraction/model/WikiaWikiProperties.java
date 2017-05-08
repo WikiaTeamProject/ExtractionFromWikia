825a698763756fce5ab625e03c06585a1b84f558
samreshperchani <samreshperchani@hotmail.com>
2017-05-09 01:31:29 +0200

    aded parameter wikiPath to save absolute path of wiki file

--- wikiaProject/src/main/java/extraction/model/WikiaWikiProperties.java
+++ wikiaProject/src/main/java/extraction/model/WikiaWikiProperties.java
@@ -9,11 +9,13 @@ public class WikiaWikiProperties {
 
     private String wikiName;
     private String languageCode;
+    private String wikiPath;
 
     //Constructor
-    public WikiaWikiProperties(String wikiName,String languageCode){
+    public WikiaWikiProperties(String wikiName,String languageCode,String wikiPath){
         this.wikiName=wikiName;
         this.languageCode=languageCode;
+        this.wikiPath=wikiPath;
     }
 
     //Getter and Setter methods for class attributes
@@ -21,8 +23,11 @@ public class WikiaWikiProperties {
 
     public String getLanguageCode(){return this.languageCode;}
 
+    public String getWikiPath(){return this.wikiPath;}
+
     public void setWikiName(String wikiName){this.wikiName=wikiName;}
 
     public void setLanguageCode(String languageCode){ this.languageCode=languageCode;}
 
+    public void setWikiPath(String wikiPath){ this.wikiPath=wikiPath;}
 }
