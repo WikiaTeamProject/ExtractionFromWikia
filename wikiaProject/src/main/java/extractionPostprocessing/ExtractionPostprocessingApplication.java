43d3aac8df6490f073de720c1415ac147c5704d4
janothan <janothan@users.noreply.github.com>
2017-05-09 15:24:52 +0200

    POM updated.

--- wikiaProject/src/main/java/extractionPostprocessing/ExtractionPostprocessingApplication.java
+++ wikiaProject/src/main/java/extractionPostprocessing/ExtractionPostprocessingApplication.java
@@ -7,26 +7,14 @@ import utils.ExtractionBz2;
 import java.io.File;
 import java.util.ResourceBundle;
 
-
+/**
+ * This applicatrion will postprocess extracted wikia wikis.
+ * Resource mappings are changed.
+ */
 public class ExtractionPostprocessingApplication {
 
     public static void main(String[] args) {
 
-
-        // Example on how to call the extractor for one file
-        /**
-        String testFile2 = "C:/Users/D060249/Desktop/TMP/GoT_Wikia_Dump/enwiki/20170331/enwiki-20170331-article-templates-nested.ttl.bz2";
-        String newFile2 = "C:/Users/D060249/Desktop";
-        ExtractionBz2.extract(testFile2, newFile2);
-        **/
-
-        // Example on how to call the extractor for multiple files
-        /**
-        String testFile3 = "C:/Users/D060249/Desktop/TMP/GoT_Wikia_Dump/enwiki/20170331/";
-        String newFile3 = "C:/Users/D060249/Desktop/got_wiki";
-
-        ExtractionBz2.extractExtractorResultFiles(testFile3, newFile3);
-         **/
 
         String pathToRootDirectory = ResourceBundle.getBundle("config").getString("pathToRootDirectory");
         File root = new File(pathToRootDirectory);
