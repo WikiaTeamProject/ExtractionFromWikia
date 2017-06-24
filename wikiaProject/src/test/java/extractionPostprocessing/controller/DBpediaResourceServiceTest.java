0af1fd8ae3b4483af4e24c988feaf6816be425ef
janothan <janothan@users.noreply.github.com>
2017-06-24 13:38:18 +0200

    Comment in test again.

--- wikiaProject/src/test/java/extractionPostprocessing/controller/DBpediaResourceServiceTest.java
+++ wikiaProject/src/test/java/extractionPostprocessing/controller/DBpediaResourceServiceTest.java
@@ -7,25 +7,25 @@ import org.junit.Test;
 /**
  * Created by D060249 on 21.06.2017.
  */
-//
-//public class DBpediaResourceServiceTest {
-//
-//    static private DBpediaResourceService dbpedia_service;
-//
-//    @BeforeClass
-//    public static void setUp(){
-//        dbpedia_service = new DBpediaResourceService();
-//    }
-//
-//    @Test
-//    public void resourceExistsInDBpedia() throws Exception {
-//    }
-//
-//    @Test
-//    public void trimTag() throws Exception {
-//        String result = dbpedia_service.trimTag("<http://dbpedia.org/resource/Game_of_Thrones_Wiki>");
-//        Assert.assertEquals(result, "http://dbpedia.org/resource/Game_of_Thrones_Wiki");
-//
-//    }
 
-//}
+public class DBpediaResourceServiceTest {
+
+    static private DBpediaResourceService dbpedia_service;
+
+    @BeforeClass
+    public static void setUp(){
+        dbpedia_service = new DBpediaResourceService();
+    }
+
+    @Test
+    public void resourceExistsInDBpedia() throws Exception {
+    }
+
+    @Test
+    public void trimTag() throws Exception {
+        String result = dbpedia_service.trimTag("<http://dbpedia.org/resource/Game_of_Thrones_Wiki>");
+        Assert.assertEquals(result, "http://dbpedia.org/resource/Game_of_Thrones_Wiki");
+
+    }
+
+}
