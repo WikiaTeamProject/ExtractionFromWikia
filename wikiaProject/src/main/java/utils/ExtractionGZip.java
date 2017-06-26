3198a4d8f0369fba83b8b9b2308c9efe084aef7d
samreshperchani <samreshperchani@hotmail.com>
2017-06-26 13:38:45 +0200

    class to extract GZip files

--- wikiaProject/src/main/java/utils/ExtractionGZip.java
+++ wikiaProject/src/main/java/utils/ExtractionGZip.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.util.ResourceBundle;
 import java.util.logging.Logger;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -12,14 +13,25 @@ import java.io.File;
 public class ExtractionGZip
 {
     private static Logger logger = Logger.getLogger(ExtractionGZip.class.getName());
+    private static String downloadedDirectoryPath = ResourceBundle.getBundle("config").getString("pathToRootDirectory") + "/downloadedWikis";
+    private String directoryExtracted;
+
+    /**
+     * Constructor for Extraction GZip Class
+     */
+    public ExtractionGZip() {
+        directoryExtracted = downloadedDirectoryPath + "/extracted/";
+        FileOperations.createDirectory(directoryExtracted);
+    }
+
 
     /**
      * This method will extract Gzip file
      * (Please note: this method assumes that gzip archive contains just one file)
      * @param gzipFile :gzip file to extract
-     * @param trgFileName : name of extracted file where
+     * @param trgFolderName : Folder to where file will be extracted
      */
-    public void extractGzipFile(String gzipFile,String trgFileName){
+    public void extractGzipFile(String gzipFile,String trgFolderName){
         try{
 
             File archiveFile=
@@ -30,12 +42,15 @@ public class ExtractionGZip
 
 
             File outputFile=new
-                    File(archiveFile.getName());
+                    File(trgFolderName+"//"+archiveFile.getName().
+                    substring(0,archiveFile.getName().lastIndexOf(".")));
+
 
             if(outputFile.exists())
                 outputFile.delete();
 
             outputFile.createNewFile();
+
 
             FileOutputStream
                     outputFileStream=new FileOutputStream(outputFile);
@@ -83,14 +98,32 @@ public class ExtractionGZip
                         srcDirectory.listFiles();
 
                 for(File file:gzipFiles){
-                    extractGzipFile(file.getAbsolutePath(),
-                            file.getAbsolutePath());
+                    if(file.getName().endsWith(".gz")) {
+                        extractGzipFile(file.getAbsolutePath(),
+                                file.getAbsolutePath());
+                    }
                 }
             }
 
         }
         catch(Exception ex){
             logger.severe(ex.getMessage());
+        }
+    }
+
+    /**
+     * Unzips all Gzip files of the wikiaDumps/downloaded/gz folder
+     * into the wikiaDumps/extractedgz folder
+     *
+     */
+    public void extractAllGZipFilesIntoDesignatedFolder() {
+        String foldergz = downloadedDirectoryPath + "/downloaded/gz";
+        File folder = new File(foldergz);
+
+        for (File filegz : folder.listFiles()) {
+            logger.info("Extraction for following gz file is started: " + filegz.getAbsolutePath());
+            extractGzipFile(filegz.getAbsolutePath(),
+                    this.directoryExtracted);
         }
     }
 }
