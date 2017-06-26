aaad8259f5a4d20294f7ffe83c036515aaf870bf
samreshperchani <samreshperchani@hotmail.com>
2017-06-26 11:37:54 +0200

    This class contain methods to extract GZip files

---
+++ wikiaProject/src/main/java/utils/ExtractionGZip.java
@@ -0,0 +1,96 @@
+package utils;
+
+import java.util.logging.Logger;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.GZIPInputStream;
+import java.io.File;
+
+/**
+ * Created by Samresh Kumar on 6/26/2017.
+ */
+public class ExtractionGZip
+{
+    private static Logger logger = Logger.getLogger(ExtractionGZip.class.getName());
+
+    /**
+     * This method will extract Gzip file
+     * (Please note: this method assumes that gzip archive contains just one file)
+     * @param gzipFile :gzip file to extract
+     * @param trgFileName : name of extracted file where
+     */
+    public void extractGzipFile(String gzipFile,String trgFileName){
+        try{
+
+            File archiveFile=
+                    new File(gzipFile);
+
+            GZIPInputStream gzipStream=
+                    new GZIPInputStream(new FileInputStream(archiveFile));
+
+
+            File outputFile=new
+                    File(archiveFile.getName());
+
+            if(outputFile.exists())
+                outputFile.delete();
+
+            outputFile.createNewFile();
+
+            FileOutputStream
+                    outputFileStream=new FileOutputStream(outputFile);
+
+            byte[] inputStreamBuffer = new byte[1024];
+            int length;
+
+            while((length = gzipStream.read(inputStreamBuffer)) != -1){
+                outputFileStream.write(inputStreamBuffer, 0, length);
+            }
+
+            //close buffers
+            outputFileStream.close();
+            gzipStream.close();
+
+        }
+        catch(Exception ex){
+            logger.severe(ex.getMessage());
+        }
+
+    }
+
+
+    /**
+     * This method will extract all GZip Files in a directory
+     * @param sourceDirectory : directory containing gzip files
+     * @param targetDirectory : directory where extracted files
+     *                        will be placed
+     */
+    public void extractAllGzipFiles(String sourceDirectory,
+                                 String targetDirectory){
+        try{
+            File srcDirectory=
+                    new File(sourceDirectory);
+
+            File trgDirectory=
+                    new File(targetDirectory);
+
+            if(!trgDirectory.exists()){
+                trgDirectory.mkdir();
+            }
+
+            if(srcDirectory.isDirectory()) {
+                File[] gzipFiles =
+                        srcDirectory.listFiles();
+
+                for(File file:gzipFiles){
+                    extractGzipFile(file.getAbsolutePath(),
+                            file.getAbsolutePath());
+                }
+            }
+
+        }
+        catch(Exception ex){
+            logger.severe(ex.getMessage());
+        }
+    }
+}
