8c5b96d815c242973f377aba95768217817f0baa
samreshperchani <samreshperchani@hotmail.com>
2017-07-17 18:40:25 +0200

    Implementation of Class Mapper 2

---
+++ wikiaProject/src/main/java/extractionPostprocessing/controller/classmapper/ClassMapper_2.java
@@ -0,0 +1,35 @@
+package extractionPostprocessing.controller.classmapper;
+
+/**
+ * Created by Samresh Kumar on 7/16/2017.
+ */
+public class ClassMapper_2 extends ClassMapper{
+
+    @Override
+    public String mapSingleClass(String classToMap) {
+
+        String resourceToMap=classToMap.toLowerCase();
+        String mappedClass="<NULL>";
+
+        if(resourceToMap.contains("/template:")){
+            System.out.println(resourceToMap.indexOf("/",resourceToMap.indexOf("/template:")+1));
+
+            if(resourceToMap.indexOf("/",resourceToMap.indexOf("/template:")+1)!=-1){
+                String propertyToMap=
+                        resourceToMap.substring(0,resourceToMap.indexOf("/template:"))
+                                + resourceToMap.substring(
+                                resourceToMap.indexOf("/",resourceToMap.indexOf("/template:")+1),
+                                resourceToMap.length());
+
+
+                String ontologyClass=propertyToMap.replace("/resource/","/ontology/");
+
+                System.out.println("Ontology Class : "+ontologyClass);
+
+                mappedClass=ontologyClass;
+
+            }
+        }
+        return mappedClass;
+    }
+}
