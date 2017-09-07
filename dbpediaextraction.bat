ed76fe17b847f3db652c908d4851b464bc560c03
janothan <janothan@users.noreply.github.com>
2017-09-07 16:17:08 +0200

    batch file deleted that is not required anymore.

--- dbpediaextraction.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set dbPediaExtFrmwrkDir=%1
-echo "Echoing Variable value"
-echo %dbPediaExtFrmwrkDir%
-REM change directory to dbpedia extraction framwork directory
-cd %dbPediaExtFrmwrkDir%
-
-REM run extractor
-mvn scala:run "-Dlauncher=extraction" "-DaddArgs=extraction.default.properties"
